c60dd523ba72d35719b050f4061b0b8851f622d9
Ryan Williams <rdubz.801smashin@gmail.com>
2016-04-13 21:48:10 -0600

    Added files via upload

---
+++ IRESTClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RESTClient
+{
+    public interface IRESTClient
+    {
+        event Action<string> PutRequest;
+        event Action<string> PostRequest;
+        event Action<string> GetRequest;
+        event Action FireClearTextBox;
+
+        void ClearTextBox();
+        string Response { set; }
+
+        string StatusCode { set; }
+    }
+}
